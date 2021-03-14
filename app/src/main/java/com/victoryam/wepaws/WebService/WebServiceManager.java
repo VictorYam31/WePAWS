9b56e0e2f76bdcbbc6cf8a377d98cc1a4e5fa775
andantino125 <andantino125@hotmail.com>
2021-03-14 13:25:17 +0800

    Toby on WebService 20210314 - updated webservice instruction

--- app/src/main/java/com/victoryam/wepaws/WebService/WebServiceManager.java
+++ app/src/main/java/com/victoryam/wepaws/WebService/WebServiceManager.java
@@ -17,10 +17,13 @@ import java.util.concurrent.Future;
 
 public class WebServiceManager {
 
-    public List<ClinicMasterModel> get_clinic_master(String clinic_name, String district, String overnight) throws ExecutionException, InterruptedException {
+    //parameter @clinic_name - "" or any text
+    //parameter @district_id - 1 to 18, can be empty or single or multiply, ""  "2"  "2,6"
+    //parameter @overnight - "" or "Y" or "N"
+    public List<ClinicMasterModel> get_clinic_master(String clinic_name, String district_id, String overnight) throws ExecutionException, InterruptedException {
 
         ExecutorService executor = Executors.newCachedThreadPool();
-        GetClinicMasterTask task = new GetClinicMasterTask(clinic_name, district, overnight);
+        GetClinicMasterTask task = new GetClinicMasterTask(clinic_name, district_id, overnight);
         Future<List<ClinicMasterModel>> future = executor.submit(task);
         List<ClinicMasterModel> clinicMasterList = future.get();
         executor.shutdown();
@@ -28,8 +31,8 @@ public class WebServiceManager {
         return clinicMasterList;
     }
 
+    //parameter @clinic_id - From 1 to 38, e.g. "1", "6", "24"
     public List<ClinicReviewModel> get_clinic_review(String clinic_id) throws ExecutionException, InterruptedException {
-
         ExecutorService executor = Executors.newCachedThreadPool();
         GetClinicReviewTask task = new GetClinicReviewTask(clinic_id);
         Future<List<ClinicReviewModel>> future = executor.submit(task);
@@ -37,12 +40,13 @@ public class WebServiceManager {
         executor.shutdown();
 
         return clinicReviewList;
-
-
     }
 
+    //parameter @clinic_id - From 1 to 38, e.g. "1", "6", "24"
+    //parameter @login - "" only
+    //parameter @review - "" or any text
+    //parameter @rating - "" or "-1" or "0" or "1"
     public String add_clinic_review(String clinic_id, String login, String review, String rating) throws ExecutionException, InterruptedException {
-
         ExecutorService executor = Executors.newCachedThreadPool();
         AddClinicReviewRatingTask task = new AddClinicReviewRatingTask(clinic_id, login, review, rating);
         Future<String> future = executor.submit(task);
@@ -52,6 +56,7 @@ public class WebServiceManager {
         return rowsAffected;
     }
 
+    //parameter @input - any text
     public List<WildSearchModel> wild_search(String input) throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newCachedThreadPool();
         WildSearchTask task = new WildSearchTask(input);
