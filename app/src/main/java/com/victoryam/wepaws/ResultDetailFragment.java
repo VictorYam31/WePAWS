04b8ad3b3a5de964c17a79340f68138053e75adc
hlm783 <HLM783@student.bham.ac.uk>
2021-04-02 18:21:32 +0800

    started write review

--- app/src/main/java/com/victoryam/wepaws/ResultDetailFragment.java
+++ app/src/main/java/com/victoryam/wepaws/ResultDetailFragment.java
@@ -179,6 +179,14 @@ public class ResultDetailFragment extends Fragment {
                 TextView viewAll = (TextView) view.findViewById(R.id.result_detail_3_view_all);
                 Button writeReview = (Button) view.findViewById(R.id.result_detail_3_write_review);
                 viewAll.setOnClickListener(new openReview());
+                writeReview.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Bundle bundle = new Bundle();
+                        bundle.putString("name", iResult.getNameForResult());
+                        Navigation.findNavController(view).navigate(R.id.action_ResultDetailFragment_to_writeReviewFragment, bundle);
+                    }
+                });
             }
 
             return view;
---
+++ app/src/main/java/com/victoryam/wepaws/WriteReviewFragment.java
@@ -0,0 +1,40 @@
+package com.victoryam.wepaws;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class WriteReviewFragment extends Fragment {
+
+    private String name;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (this.getArguments() != null) {
+            this.name = this.getArguments().getString("name");
+        }
+        else {
+            Log.v("empty arguments", "no name?");
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.write_review, container, false);
+
+        TextView title = (TextView) view.findViewById(R.id.write_review_title);
+        title.setText(getResources().getString(R.string.write_review_title) + this.name);
+
+        return view;
+    }
+
+}
